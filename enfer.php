149cb056870357e5ae7c824d1e9c3f991c953bef
Sick <chezsick@bbox.fr>
2019-01-14 02:14:11 +0100

    add sick's hell

---
+++ enfer.php
@@ -0,0 +1,21 @@
+<?php
+require("randlib.php");
+$n = 0;
+$gain = 10000;
+echo "Les Gain Affichier Sur Les Jours Sont Des Gain Perdu, Vous Gagner Vos Reste<br/>";
+$jour = ["Lundi","Mardi","Mercdredi","Jeudi","Vendredi","Samdi","Dimance"];
+while ($gain > 0 && $n <= 7)
+{
+$nego = srcrand(0,30001);
+$gain = $gain - $nego;
+echo "Il Est ".$jour[$n]." J'ai Perdu ::: ".$nego.", Il Me Reste :::: ".$gain."<br/>";
+$n = $n + 1;
+}
+if ($gain > 0)
+{
+echo "Vous Avez Gagner :::::: ".$gain;
+}
+else;
+{
+echo "Vous Avez Perdu";
+}
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require("config.php");
 echo "<h1>Bienvenue Dans RandomLike Des Jeux !</h1>";
-echo "Vous Avez Le Choix De Mes Jeux ::::: <br/><a href='intersect.php'>L'intersection - 5000</a></br>";
-echo "Choisissez Vos Numero Entre 1 et 40 Separer Avec Espace ::: <form form action='loto.php' method='get'><input type='text' name='num'></form>";
+echo "Vous Avez Le Choix De Mes Jeux ::::: <br/><a href='intersect.php'>L'intersection - 5000</a></br><a href='enfer.php'>L'enfer De Sick - 10000</a></br>";
+echo "Choisissez 6 Numero Entre 1 et 40 Separer Avec Espace ::: <form form action='loto.php' method='get'><input type='text' name='num'></form>";
 $count = file_get_contents($randomsourcecount);
 echo "<br/>Le Site Des Jeux A Générer ::::::: " . $count;
