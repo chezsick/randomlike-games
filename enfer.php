602d517b18896a02292d0435dcbea8a107260fd4
Sick <chezsick@bbox.fr>
2019-01-14 02:32:19 +0100

    update sum to 100 000 at hell

--- enfer.php
+++ enfer.php
@@ -1,12 +1,12 @@
 <?php
 require("randlib.php");
 $n = 0;
-$gain = 10000;
+$gain = 100000;
 echo "Les Gain Affichier Sur Les Jours Sont Des Gain Perdu, Vous Gagner Vos Reste<br/>";
 $jour = ["Lundi","Mardi","Mercdredi","Jeudi","Vendredi","Samdi","Dimance"];
-while ($gain > 0 && $n <= 7)
+while (($gain > 0) && ($n < 7))
 {
-$nego = srcrand(0,30001);
+$nego = srcrand(0,100001);
 $gain = $gain - $nego;
 echo "Il Est ".$jour[$n]." J'ai Perdu ::: ".$nego.", Il Me Reste :::: ".$gain."<br/>";
 $n = $n + 1;
@@ -14,8 +14,4 @@ $n = $n + 1;
 if ($gain > 0)
 {
 echo "Vous Avez Gagner :::::: ".$gain;
-}
-else;
-{
-echo "Vous Avez Perdu";
 }
